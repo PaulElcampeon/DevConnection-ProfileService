5e3d97e12e8bdcb8d78562fcac611c734b988760
PaulElcampeon <pauloladele@live.co.uk>
2019-08-26 02:31:19 +0100

    added new generic response

---
+++ src/main/java/com/devconnection/ProfileService/messages/GenericResponse.java
@@ -0,0 +1,13 @@
+package com.devconnection.ProfileService.messages;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenericResponse {
+
+    boolean success;
+}
